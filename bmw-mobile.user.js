3ae3a8722b984c91781af3761e395bda427701d2
KTG-kr <xorlarbs1990@gmail.com>
2026-06-09 14:06:42 +0900

    Update bmw-mobile.user.js

--- bmw-mobile.user.js
+++ bmw-mobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BMW Auto Mobile
 // @namespace    https://github.com/KTG-kr/bmwauto
-// @version      1.2
+// @version      1.3
 // @match        https://*.bmw.co.kr/*
 // @match        https://*.bmwgroup.com/*
 // @match        https://*.bmw.com/*
