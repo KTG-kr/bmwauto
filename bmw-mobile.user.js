35649b92265f8b060003c5e5bfe08cc00518c948
KTG-kr <xorlarbs1990@gmail.com>
2026-06-15 14:00:39 +0900

    Update bmw-mobile.user.js

--- bmw-mobile.user.js
+++ bmw-mobile.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BMW Auto Mobile
 // @namespace    https://github.com/KTG-kr/bmwauto
-// @version      1.5
+// @version      1.8
 // @match        https://*.bmw.co.kr/*
 // @match        https://*.bmwgroup.com/*
 // @match        https://*.bmw.com/*
@@ -35,20 +35,6 @@
   btn.style.cssText = [
     "position:fixed",
     "right:12px",
-    "bottom:76px",
-    "z-index:2147483647",
-    "background:#ff354f",
-    "color:#fff",
-    "border:0",
-    "border-radius:10px",
-    "padding:12px 14px",
-    "font:bold 14px sans-serif",
-    "box-shadow:0 3px 12px rgba(0,0,0,.25)"
-  ].join(";");
-
-  btn.onclick = run;
-  document.documentElement.appendChild(btn);
-})();    "right:12px",
     "bottom:76px",
     "z-index:2147483647",
     "background:#ff354f",
